cf510cc30a7d5bbfebc70a174a92cd940bd1473a
Sam Hall <mann.samantha11@gmail.com>
2020-11-30 21:33:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   minimum_value = 0
-  min_hash_value = {}
+  min_hash_value = nil
   name_hash.each do |name, value|
     if minimum_value == 0 || value < minimum_value
       minimum_value = value
